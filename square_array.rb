380e9ba24e5ae0353db2a7af18ab7bebe84bda6f
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-08-30 12:45:36 -0700

    completed assignment

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  new_array = []
+
+  array.each do |num|
+    new_array << num * num
+  end
+  new_array
 end
+
+
+# #Collect iterates over each element and returns a new array 
+# def square_array(array)
+#   array.collect { |num| num * num}
+# end
+# binding.pry
+
